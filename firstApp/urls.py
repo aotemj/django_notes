ca2b6490d9c0362d5a528c24d726dc757a12c93a
losidk <losidkl@gmail.com>
2022-09-27 06:57:44 +0800

    url parameters example

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -2,5 +2,12 @@ from firstApp import views
 from django.urls import path
 
 urlpatterns = [
-    path("login/", views.first_app_login)
+    path("login/", views.first_app_login),
+    path("article/", views.article_list),
+    # dynamic parameter
+    path("article/<int:article_id>/", views.article),
+    # multiple dynamic parameter
+    path("article/<int:article_id>/<str:section_name>", views.article_section),
+    # slug type: word、number and _ -
+    path("article/<slug:slug_parameter>", views.article_search)
 ]
--- firstApp/views.py
+++ firstApp/views.py
@@ -20,3 +20,22 @@ def get_setting_config(request):
 
 def first_app_login(request):
     return HttpResponse("This is a example that import urls from the app")
+
+
+def article_list(request):
+    return HttpResponse("This is a example of fetch the article list")
+
+
+def article(request, article_id):
+    return HttpResponse(
+        f"This is a example of fetch the specific article by articleId, current articleId is : {article_id}")
+
+
+def article_section(request, article_id, section_name):
+    return HttpResponse(
+        f"This is a example of dynamic multiple parameter: first parameter is article_id:{article_id}, second parameter is section_name: {section_name}")
+
+
+def article_search(request, slug_parameter):
+    return HttpResponse(
+        f"This is a example of the slug parameter, a slug parameter means the parameter contains 'number' 、'word'、'-' 、'_',and the slug parameter is : {slug_parameter}")
