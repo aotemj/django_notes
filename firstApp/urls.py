4654ba443b614bd366030f94d5197acb8d8cd0d2
losidk <losidkl@gmail.com>
2022-09-30 05:58:21 +0800

    using example of the regexp path

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -1,25 +1,28 @@
 from firstApp import views
-from django.urls import path, register_converter
+from django.urls import path, register_converter, re_path
 from . import conventers
 
 # register custom converter
 register_converter(conventers.MyYearConverter, 'yyyy')
 
 urlpatterns = [
-    path("login/", views.first_app_login),
-    path("article/", views.article_list),
-    # dynamic parameter
-    path("article/<int:article_id>/", views.article),
-    # multiple dynamic parameter
-    path("article/<int:article_id>/<str:section_name>/", views.article_section),
-    # slug type: word、number and _ -
-    path("article/<slug:slug_parameter>/", views.article_search),
-    # path type: include /
-    path("article/path/<path:path>/", views.article_path),
-    # str type: not include /
-    path("article/<str:str>/", views.article_str),
-    # uuid type
-    path("article/<uuid:uuid>", views.article_uuid),
-    #     use the custom converter
-    path("<yyyy:year>", views.custom_year_converter)
+    # path("login/", views.first_app_login),
+    # path("article/", views.article_list),
+    # # dynamic parameter
+    # path("article/<int:article_id>/", views.article),
+    # # multiple dynamic parameter
+    # path("article/<int:article_id>/<str:section_name>/", views.article_section),
+    # # slug type: word、number and _ -
+    # path("article/<slug:slug_parameter>/", views.article_search),
+    # # path type: include /
+    # path("article/path/<path:path>/", views.article_path),
+    # # str type: not include /
+    # path("article/<str:str>/", views.article_str),
+    # # uuid type
+    # path("article/<uuid:uuid>", views.article_uuid),
+    # #     use the custom converter
+    # path("<yyyy:year>", views.custom_year_converter),
+    #     regexp path
+    re_path(r'^articles/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<slug>[\w-]+)/$',
+            views.article_regexp_year_month_slug)
 ]
--- firstApp/views.py
+++ firstApp/views.py
@@ -19,39 +19,43 @@ def get_setting_config(request):
 
 
 def first_app_login(request):
-    return HttpResponse("This is a example that import urls from the app")
+    return HttpResponse("This is an example that import urls from the app")
 
 
 def article_list(request):
-    return HttpResponse("This is a example of fetch the article list")
+    return HttpResponse("This is an example of fetch the article list")
 
 
 def article(request, article_id):
     return HttpResponse(
-        f"This is a example of fetch the specific article by articleId, current articleId is : {article_id}")
+        f"This is an example of fetch the specific article by articleId, current articleId is : {article_id}")
 
 
 def article_section(request, article_id, section_name):
     return HttpResponse(
-        f"This is a example of dynamic multiple parameter: first parameter is article_id:{article_id}, second parameter is section_name: {section_name}")
+        f"This is an example of dynamic multiple parameter: first parameter is article_id:{article_id}, second parameter is section_name: {section_name}")
 
 
 def article_search(request, slug_parameter):
     return HttpResponse(
-        f"This is a example of the slug parameter, a slug parameter means the parameter contains 'number' 、'word'、'-' 、'_',and the slug parameter is : {slug_parameter}")
+        f"This is an example of the slug parameter, a slug parameter means the parameter contains 'number' 、'word'、'-' 、'_',and the slug parameter is : {slug_parameter}")
 
 
 def article_path(request, path):
-    return HttpResponse(f"This is a example of the path type, the path is: {path}")
+    return HttpResponse(f"This is an example of the path type, the path is: {path}")
 
 
 def article_str(request, str):
-    return HttpResponse(f"This is a example of the str type, the str is: {str}")
+    return HttpResponse(f"This is an example of the str type, the str is: {str}")
 
 
 def article_uuid(request, uuid):
-    return HttpResponse(f"This is a example of the uuid, the uuid is:{uuid}")
+    return HttpResponse(f"This is an example of the uuid, the uuid is:{uuid}")
 
 
 def custom_year_converter(request, year):
-    return HttpResponse(f"This is a example of the custom converter, the year is:{year}")
+    return HttpResponse(f"This is an example of the custom converter, the year is:{year}")
+
+
+def article_regexp_year_month_slug(request, year, month, slug):
+    return HttpResponse(f"This is an example the year is :{year} month: {month}, slug: {slug}")
